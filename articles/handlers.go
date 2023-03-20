2ba8217db79837a4836924d5d549e72c9ed688c2
onee-only <kimww0306@gmail.com>
2023-03-20 19:40:50 +0900

    handler 정의 및 middleware 추가

---
+++ articles/handlers.go
@@ -0,0 +1,17 @@
+package articles
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetArticle(c *fiber.Ctx) error {
+}
+
+func CreateArticle(c *fiber.Ctx) error {
+}
+
+func UpdateArticle(c *fiber.Ctx) error {
+}
+
+func DeleteArtcile(c *fiber.Ctx) error {
+}
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/articles"
+	"github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common"
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
-	
+
+	// server
+	app := fiber.New()
+	{
+		// route
+
+		// ping pong
+		app.Get("ping", func(c *fiber.Ctx) error {
+			return c.Status(http.StatusOK).JSON(fiber.Map{"message": "pong"})
+		})
+
+		// articles
+		articlesRouter := app.Group("articles")
+		articlesRouter.Use(common.GetParamMiddleWare)
+		// TODO : create auth middleware
+		{
+			articlesRouter.Get(":id", articles.GetArticle)
+			articlesRouter.Post(":id", articles.CreateArticle)
+			articlesRouter.Put(":id", articles.UpdateArticle)
+			articlesRouter.Delete(":id", articles.DeleteArtcile)
+		}
+
+		app.Listen(":8080")
+	}
 }
