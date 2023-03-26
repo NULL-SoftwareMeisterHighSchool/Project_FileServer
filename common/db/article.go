a04d265e0d293a9f2c407bdaf9584476e917331e
onee-only <kimww0306@gmail.com>
2023-03-26 17:10:49 +0900

    Sanitizer 세팅

--- common/db/article.go
+++ common/db/article.go
@@ -1,4 +1,20 @@
 package db
 
 type Article struct {
+	ID        uint     `json:"id"`
+	Summary   string   `json:"summary"`
+	Thumbnail string   `json:"thumbnail"`
+	Images    []string `json:"-"`
+}
+
+func (a *Article) SetSummaryFromBody(body []byte) {
+
+}
+
+func (a *Article) SetImagesFromBody(body []byte) {
+
+}
+
+func (a *Article) SetThumbnail() {
+
 }
