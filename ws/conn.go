9251e01f00a77d67cc119447544757e69b9c739a
onee-only <kimww0306@gmail.com>
2023-04-04 19:37:22 +0900

    DELETE: 웹소켓 conn 함수 주석 처리

--- ws/conn.go
+++ ws/conn.go
@@ -4,23 +4,23 @@ import (
 	"github.com/gofiber/websocket/v2"
 )
 
-var requestMap = make(map[string]chan interface{})
+// var requestMap = make(map[string]chan interface{})
 
 func Connect(c *websocket.Conn) {
 	// TODO: create map of request ids so that i can organize my requests
 
-	go read(c)
-	go write(c)
+	// go read(c)
+	// go write(c)
 }
 
-func read(c *websocket.Conn) {
-	for {
-		c.ReadJSON()
-	}
-}
+// func read(c *websocket.Conn) {
+// 	for {
+// 		c.ReadJSON()
+// 	}
+// }
 
-func write(c *websocket.Conn) {
-	for {
-		c.WriteJSON()
-	}
-}
+// func write(c *websocket.Conn) {
+// 	for {
+// 		c.WriteJSON()
+// 	}
+// }
