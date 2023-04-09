5ad544a1c4cfb84b7dc404e8504449f61045dcd9
onee-only <kimww0306@gmail.com>
2023-04-09 19:26:02 +0900

    ADD: defer문 추가

--- ws/conn.go
+++ ws/conn.go
@@ -9,18 +9,20 @@ import (
 func Connect(c *websocket.Conn) {
 	// TODO: create map of request ids so that i can organize my requests
 
-	// go read(c)
-	// go write(c)
+	go read(c)
+	go write(c)
 }
 
-// func read(c *websocket.Conn) {
-// 	for {
-// 		c.ReadJSON()
-// 	}
-// }
+func read(c *websocket.Conn) {
+	defer c.Close()
+	for {
+		c.ReadJSON()
+	}
+}
 
-// func write(c *websocket.Conn) {
-// 	for {
-// 		c.WriteJSON()
-// 	}
-// }
+func write(c *websocket.Conn) {
+	defer c.Close()
+	for {
+		c.WriteJSON()
+	}
+}
