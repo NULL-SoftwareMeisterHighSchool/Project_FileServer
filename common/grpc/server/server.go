5ce60994abbc3a8eb88db11954c6674fd153ea9b
onee-only <kimww0306@gmail.com>
2023-04-27 20:33:55 +0900

    ADD: 구현된 서버 register

--- common/grpc/server/server.go
+++ common/grpc/server/server.go
@@ -7,10 +7,13 @@ import (
 
 	"github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/config"
 	articles_server "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/articles"
+	comments_server "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/comments"
 	articles_pb "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/pb/articles"
 	comments_pb "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/pb/comments"
 	rank_pb "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/pb/rank"
 	users_pb "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/pb/users"
+	rank_server "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/rank"
+	users_server "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/users"
 	"google.golang.org/grpc"
 )
 
@@ -23,9 +26,9 @@ func Listen() {
 	grpcServer := grpc.NewServer()
 
 	articles_pb.RegisterArticleServiceServer(grpcServer, articles_server.ArticleServiceServer{})
-	comments_pb.RegisterCommentServiceServer(grpcServer, comments_pb.UnimplementedCommentServiceServer{})
-	users_pb.RegisterUserEventServiceServer(grpcServer, users_pb.UnimplementedUserEventServiceServer{})
-	rank_pb.RegisterRankServiceServer(grpcServer, rank_pb.UnimplementedRankServiceServer{})
+	comments_pb.RegisterCommentServiceServer(grpcServer, comments_server.CommentServiceServer{})
+	users_pb.RegisterUserEventServiceServer(grpcServer, users_server.UserServiceServer{})
+	rank_pb.RegisterRankServiceServer(grpcServer, rank_server.RankServiceServer{})
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatal(err)
