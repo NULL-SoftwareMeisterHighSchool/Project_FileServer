9b63ee880c1725856f98639a2695b43b1c80cf03
onee-only <kimww0306@gmail.com>
2023-04-27 20:31:22 +0900

    ADD: GetUsersContribution 구현

--- common/grpc/server/rank/service.go
+++ common/grpc/server/rank/service.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	pb "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/pb/rank"
+	"github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/domain/rank"
 )
 
 type RankServiceServer struct {
 	pb.UnimplementedRankServiceServer
 }
 
-func (RankServiceServer) GetContributionCount(context.Context, *pb.GetContributionCountRequest) (*pb.GetCountributionCountResponse, error) {
-	
+func (RankServiceServer) GetContributionCount(ctx context.Context, request *pb.GetContributionCountRequest) (*pb.GetCountributionCountResponse, error) {
+	return rank.GetUsersContribution(request.GetUsers())
 }
