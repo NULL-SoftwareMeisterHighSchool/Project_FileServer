ac7e0c2eed216c071635bf14f5b5bd5d42d9d6c8
onee-only <kimww0306@gmail.com>
2023-04-28 10:39:45 +0900

    CHORE

--- common/grpc/server/users/service.go
+++ common/grpc/server/users/service.go
@@ -4,16 +4,30 @@ import (
 	"context"
 
 	pb "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/grpc/server/pb/users"
+	"github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/domain/users"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UserServiceServer struct {
 	pb.UnimplementedUserEventServiceServer
 }
 
-func (UserServiceServer) PublishUserCreated(context.Context, *pb.CreateUserEvent) (*pb.Nothing, error) {
+func (UserServiceServer) PublishUserCreated(ctx context.Context, event *pb.CreateUserEvent) (*empty.Empty, error) {
 
+	if err := users.CreateUser(uint(event.GetUserID())); err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	return &empty.Empty{}, nil
 }
 
-func (UserServiceServer) PublishUserDeleted(context.Context, *pb.DeleteUserEvent) (*pb.Nothing, error) {
+func (UserServiceServer) PublishUserDeleted(ctx context.Context, event *pb.DeleteUserEvent) (*empty.Empty, error) {
 
+	if err := users.DeleteUser(uint(event.GetUserID())); err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	return &empty.Empty{}, nil
 }
