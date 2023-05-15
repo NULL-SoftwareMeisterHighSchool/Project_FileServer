11962bba0a846a56580955e42674d6e525b44021
onee-only <kimww0306@gmail.com>
2023-05-15 23:32:17 +0900

    ADD: e2e test에 Teardown 추가

--- common/database/conn.go
+++ common/database/conn.go
@@ -20,6 +20,7 @@ var (
 	Users         *gorm.DB
 	Comments      *gorm.DB
 	ArticleLikes  *gorm.DB
+	DB            *gorm.DB
 )
 
 func Connect() {
@@ -32,6 +33,8 @@ func Connect() {
 	if err != nil {
 		log.Fatalf("cannot open db: %s", err)
 	}
+
+	DB = db
 
 	migrateDB(db)
 	loadTables(db)
--- common/test/e2e/teardown.go
+++ common/test/e2e/teardown.go
@@ -1,7 +1,28 @@
 package e2e_test
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/database"
+	article_entity "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/database/articles/entity"
+	comment_entity "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/database/comments/entity"
+	image_entity "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/database/images/entity"
+	user_entity "github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/database/users/entity"
+)
 
 func TearDown(t *testing.T) {
-	
+	if err := database.DB.Migrator().DropTable(
+		&article_entity.Article{},
+		&article_entity.ArticleBody{},
+		&comment_entity.Comment{},
+		&image_entity.Image{},
+		&user_entity.User{},
+		"user_likes",
+	); err != nil {
+		t.Fatalf("failed to drop tables: %v", err)
+	}
+
+	// if storage uses filesystem
+	os.RemoveAll("./contents")
 }
