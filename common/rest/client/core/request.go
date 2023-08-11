9ee2fa7be96cb5b20f16447c9523466894346d3d
onee-only <kimww0306@gmail.com>
2023-08-11 14:54:03 +0900

    FIX: auth 로직 수정

--- common/rest/client/core/request.go
+++ common/rest/client/core/request.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/NULL-SoftwareMeisterHighSchool/Project_FileServer/common/config"
@@ -13,31 +12,29 @@ import (
 var client = &http.Client{}
 
 type userInfoResp struct {
-	id   int
-	name string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func GetUserIDFromToken(accessToken string) uint {
 	req, _ := http.NewRequest("GET", fmt.Sprintf("http://%s:3000/users/me/tiny", config.CORE_IP), nil)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 
-	log.Println(req)
 	resp, err := client.Do(req)
 	if err != nil || resp.StatusCode != http.StatusOK {
 		return 0
 	}
 	defer resp.Body.Close()
 
-	result := new(userInfoResp)
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-	log.Println(data)
 
-	if err = json.Unmarshal(data, result); err != nil {
+	var result userInfoResp
+	if err = json.Unmarshal(data, &result); err != nil {
 		panic(err)
 	}
 
-	return uint(result.id)
+	return uint(result.Id)
 }
